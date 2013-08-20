df707c9b4a074a5cf8515338fc7658d314979bec
andycasey <andycasey@gmail.com>
2013-08-20 23:03:23 +1000

    Test default configuration

--- scope/tests.py
+++ scope/tests.py
@@ -1,5 +1,27 @@
+# coding: utf-8
+
+""" SCOPE tests. """
+
+from __future__ import division, print_function
+
+__author__ = "Andy Casey <acasey@mso.anu.edu.au>"
+
+# Standard library
+import logging
+import os
+import re
+
+from glob import glob
+
+# Third-party
+import numpy as np
 import pyfits
 import yaml
+
+# Module imports
+import config
+
+folder_path = os.path.abspath(os.path.dirname(__file__))
 
 def test_assert():
     assert True
@@ -7,4 +29,26 @@ def test_assert():
 
 def test_scipy():
     from scipy.interpolate import griddata
-    
+
+
+def test_default_configuration():
+        
+    configuration = config.load(os.path.join(folder_path, '../config.yml'))
+
+    # Verify everything except models
+
+    # Check the normalisation
+    normalisation_priors = verify_normalisation(configuration)
+
+    # Check the smoothing
+    smoothing_priors = verify_smoothing(configuration)
+
+    # Check the doppler corrections
+    doppler_priors = verify_doppler(configuration)
+
+    # Establish all of the priors    
+    priors_to_expect = doppler_priors + smoothing_priors + normalisation_priors
+
+    # Verify that we have priors established for all the priors
+    # we expect, and the stellar parameters we plan to solve for
+    verify_priors(configuration, priors_to_expect)
