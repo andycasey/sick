7be2447174465489d8d37d6a6990d79940f046eb
andycasey <andycasey@gmail.com>
2013-09-09 17:08:35 +1000

    Visualise results from SCOPE

--- scope/visualise.py
+++ scope/visualise.py
@@ -6,10 +6,14 @@ from __future__ import division, print_function
 
 __author__ = "Andy Casey <acasey@mso.anu.edu.au>"
 
+# Standard libraries
+import logging
+
 # Third-party
 import matplotlib.pyplot as plt
 import numpy as np
 
+__all__ = ['animate_setup', 'animate_callback', 'plot_all_results', 'plot_result']
 
 def animate_setup(num_spectra):
     """Creates a figure and axes for the animate_callback() function
@@ -58,3 +62,59 @@ def animate_callback(total_chi_sq, num_dof, parameters, observed_spectra, model_
     # Ensure everything is drawn
     plt.draw()
     plt.pause(0.01)
+
+
+def plot_result(chi_sq, num_dof, posteriors, observed_spectra, model_spectra):
+    """Plots the SCOPE result and returns the figure canvas."""
+
+    if posteriors is None:
+        return None
+
+    fig = plt.figure()
+    num_spectra = len(observed_spectra)
+
+    axis = fig.add_subplot(num_spectra + 1, 1, 1)
+    axis.axis('off')
+    axis.set_title('$\chi^2$ = {chi_sq:.2f}, $N_{{dof}}$ = {num_dof:.0f}'
+        .format(chi_sq=chi_sq, num_dof=num_dof))
+ 
+    posteriors_formatted = "\n".join(["{parameter}:  {value:.3f}".format(parameter=parameter, value=posteriors[parameter])
+        for parameter in sorted(posteriors.keys(), key=len)])
+
+    axis.text(0.05, 0.05, posteriors_formatted, fontsize='x-small', transform=axis.transAxes)
+
+    for i, (observed_spectrum, model_spectrum) in enumerate(zip(observed_spectra, model_spectra), start=2):
+        axis = fig.add_subplot(num_spectra + 1, 1, i)
+
+        axis.plot(model_spectrum.disp, model_spectrum.flux, 'b', zorder=1)
+        axis.plot(observed_spectrum.disp, observed_spectrum.flux, 'k', zorder=5)
+
+        # Get the overlap
+        xlim = [
+            max(observed_spectrum.disp[0], model_spectrum.disp[np.where(np.isfinite(model_spectrum.flux) == True)[0][0]]),
+            min(observed_spectrum.disp[-1], model_spectrum.disp[np.where(np.isfinite(model_spectrum.flux) == True)[0][-1]])
+        ]
+        axis.set_xlim(xlim)
+        axis.set_ylim(0, 1.2)
+
+        axis.set_ylabel('Normalised Flux')
+        if i == num_spectra + 1:
+            axis.set_xlabel('Wavelength (${\AA}$)')
+        
+
+    return fig
+
+
+def plot_all_results(results, output_prefix):
+    """Plots and saves each result to file."""
+
+    for i, result in enumerate(results, start=1):
+        
+        fig = plot_result(*result)
+        if fig is None: continue
+
+        filename = '{output_prefix}-{i}.png'.format(output_prefix=output_prefix, i=i)
+        logging.info("Saved plotted result to {filename}".format(filename=filename))
+
+        plt.savefig(filename)
+        plt.close('all')
