a57e361c184afbc15ce83d4a51df83a5017f3a46
Andy Casey <andycasey@gmail.com>
2014-05-25 23:42:18 +0100

    version bump

--- sick/__init__.py
+++ sick/__init__.py
@@ -3,7 +3,7 @@
 """ Spectroscopic inference of astrophysical quantities """
 
 __author__ = "Andy Casey <arc@ast.cam.ac.uk>"
-__version__ = "0.09dev"
+__version__ = "0.091dev"
 
 __all__ = ["config", "Model", "specutils", "Spectrum", "utils"]
 
