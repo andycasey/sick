6b081f10766a6691d3e65b76c51dd64334a577a8
Andy Casey <andycasey@gmail.com>
2014-06-04 15:31:26 +0100

    Added a *sick* inference test

--- sick/tests/test_inference.py
+++ sick/tests/test_inference.py
@@ -6,7 +6,10 @@ from __future__ import division, print_function
 
 __author__ = "Andy Casey <andy@ast.cam.ac.uk>"
 
+import gzip
+import os
 import unittest
+import urllib
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,27 +18,22 @@ from matplotlib.ticker import MaxNLocator
 import sick
 import triangle
 
+TEST_DATA_URL = "http://astrowizici.st/test-inference-data.tar.gz"
+
 # This world needs a little more truthiness.
 truth = {
-    "teff": 3810,
-    "logg": 0.403,
+    "teff": 5454,
+    "logg": 4.124,
     "feh": -0.514,
-    "alpha": 0.12,
-    "convolve.blue": 1.734,
-    "convolve.red": 0.671,
-    "z.blue": 56.12/299792458e-3,
-    "z.red": 64.34/299792458e-3,
-    "jitter.blue": -2.321, # ~ 10% underestimated
-    "jitter.red": -5.418,  # < 1% underestimated
+    "alpha": 0.02,
+    "convolve.blue": 0.34,
+    "z.blue": -56.12/299792458e-3,
+    "jitter.blue": np.log(0.10), # ~10% underestimated
     "normalise.blue.c0": 0.00123,
     "normalise.blue.c1": -0.5934,
     "normalise.blue.c2": -423.18,
-    "normalise.red.c0": -0.00837,
-    "normalise.red.c1": 49.741,
-    "normalise.red.c2": -212763.37,
 }
 
-@unittest.skip("Model information not hosted online yet.")
 class InferenceTest(unittest.TestCase):
 
     def setUp(self):
@@ -43,9 +41,12 @@ class InferenceTest(unittest.TestCase):
         Download the model information and initialise it.
         """
 
-        # TODO: Download the model information
+        # Download the data that we need
+        urllib.urlretrieve(TEST_DATA_URL, "test-inference-data.tar.gz")
 
-        self.model = sick.models.Model("model.yaml")
+        # Uncompress the data
+        os.system("gunzip test-inference-data.tar.gz")
+        os.system("tar -xzf test-inference-data.tar")
 
 
     def runTest(self):
@@ -53,16 +54,19 @@ class InferenceTest(unittest.TestCase):
         Create a faux spectrum then infer the model parameters given the faux data.
         """
 
+        # Initialise the model
+        model = sick.models.Model("inference-model.yaml")
+
         # We create a faux-faux observation just so our faux observations get mapped
         # back onto the model.dispersion once they have been redshifted
-        faux_obs = [sick.specutils.Spectrum1D(disp=self.model.dispersion[channel],
-            flux=np.zeros(len(self.model.dispersion[channel]))) \
-            for channel in self.model.channels]
-        fluxes = self.model(observations=faux_obs, **truth)
+        faux_obs = [sick.specutils.Spectrum1D(disp=model.dispersion[channel],
+            flux=np.zeros(len(model.dispersion[channel]))) \
+            for channel in model.channels]
+        fluxes = model(observations=faux_obs, **truth)
 
         observations = []
-        for channel, flux in zip(self.model.channels, fluxes):
-            disp = self.model.dispersion[channel]
+        for channel, flux in zip(model.channels, fluxes):
+            disp = model.dispersion[channel]
 
             N = len(disp)
             flux_err = 0.1 + 0.5 * np.random.randn(N)
@@ -71,23 +75,22 @@ class InferenceTest(unittest.TestCase):
             flux += np.abs(jitter_true*flux) * np.random.randn(N)
             flux += flux_err * np.random.randn(N)
 
-            # Apply the Pb, Vb
-
-            # Throw away half of the data. Just for fun.
+            # Now let's throw away half of the data just for fun
             spectrum = sick.specutils.Spectrum1D(disp=disp[::2], flux=flux[::2],
                 variance=flux_err[::2]**2)
             observations.append(spectrum)
 
-        # Now let's solve for the self.model parameters
-        posteriors, sampler, info = sick.solve(observations, self.model)
+        # Now let's solve for the model parameters
+        posteriors, sampler, info = sick.solve(observations, model)
 
         # Plot the chains
-        ndim = len(self.model.dimensions)
+        ndim = len(model.dimensions)
         chain_to_plot = sampler.chain.reshape(-1, ndim)
-        chains_per_plot = len(self.model.grid_points.dtype.names)
+        chains_per_plot = len(model.grid_points.dtype.names)
 
         self.n, subplots = 1, 4
-        for j, dimension in enumerate(self.model.dimensions):
+        steps = model.configuration["solver"]["burn"] + model.configuration["solver"]["sample"]
+        for j, dimension in enumerate(model.dimensions):
 
             if j % subplots == 0:
                 if j > 0:
@@ -98,37 +101,41 @@ class InferenceTest(unittest.TestCase):
                 fig = plt.figure()
 
             ax = fig.add_subplot(subplots, 1, (1 + j) % subplots)
-            for k in range(self.model.configuration["solver"]["walkers"]):
+            for k in range(model.configuration["solver"]["walkers"]):
                 ax.plot(range(1, 1 + len(info["mean_acceptance_fractions"])),
                     info["chain"][k, :, j], c="k", alpha=0.5)
-            ax.axvline(self.model.configuration["solver"]["burn"], ymin=0, ymax=1,
+            ax.axvline(model.configuration["solver"]["burn"], ymin=0, ymax=1,
                 linestyle=":", c="k")
             ax.set_ylabel(sick.utils.latexify([dimension])[0])
             ax.yaxis.set_major_locator(MaxNLocator(4))
-            ax.set_xlim(0, ax.get_xlim()[1])
+    
+            # Plot the truth
+            ax.plot([0, steps], [truth[dimension], truth[dimension]], lw=2, c="#4682b4", zorder=10)
+            ax.set_xlim(0, steps)
 
         [axes.xaxis.set_ticklabels([]) for axes in fig.axes[:-1]]
         ax.set_xlabel("Iteration")
         fig.savefig("chain-{0}.pdf".format(self.n))
 
         # Make a corner plot with just the parameters of interest
-        psi_len = len(self.model.grid_points.dtype.names)
-        fig = triangle.corner(sampler.chain.reshape(-1, len(self.model.dimensions))[:, :psi_len],
-            labels=sick.utils.latexify(self.model.grid_points.dtype.names), quantiles=[.16, .50, .84], verbose=False,
-            truths=[truth[dimension] for dimension in self.model.dimensions[:psi_len]], extents=[0.95]*psi_len)
+        psi_len = len(model.grid_points.dtype.names)
+        fig = triangle.corner(sampler.chain.reshape(-1, len(model.dimensions))[:, :psi_len],
+            labels=sick.utils.latexify(model.grid_points.dtype.names), quantiles=[.16, .50, .84], verbose=False,
+            truths=[truth[dimension] for dimension in model.dimensions[:psi_len]], extents=[0.95]*psi_len)
         fig.savefig("inference.pdf")
 
-        # Make a corner plot with *all* of the self.model parameters
-        fig = triangle.corner(sampler.chain.reshape(-1, len(self.model.dimensions)),
-            labels=sick.utils.latexify(self.model.dimensions), quantiles=[.16, .50, .84], verbose=False,
-            truths=[truth[dimension] for dimension in self.model.dimensions])
+        # Make a corner plot with *all* of the model parameters
+        fig = triangle.corner(sampler.chain.reshape(-1, len(model.dimensions)),
+            labels=sick.utils.latexify(model.dimensions), quantiles=[.16, .50, .84], verbose=False,
+            truths=[truth[dimension] for dimension in model.dimensions])
         fig.savefig("inference-all.pdf")
 
-        # Assert that we have found a 'pretty good' solution.
-        acceptable_ci_multiple = 1.5
-        for dimension in self.model.dimensions:
+        # Assert that we have found a solution at least within 2-sigma
+        acceptable_ci_multiple = 2.
+        for dimension in model.dimensions:
             peak_posterior, pos_ci, neg_ci = posteriors[dimension]
-            assert (peak_posterior + acceptable_ci_multiple * pos_ci >= truth[dimension] >= peak_posterior + acceptable_ci_multiple * neg_ci)
+            assert (peak_posterior + acceptable_ci_multiple * pos_ci >= truth[dimension] >= peak_posterior - acceptable_ci_multiple * neg_ci), (
+                "Inferences on the test case were not within {0}-sigma of the truth values".format(acceptable_ci_multiple))
 
 
     def tearDown(self):
@@ -136,9 +143,19 @@ class InferenceTest(unittest.TestCase):
         Remove the downloaded files, and remove the created figures.
         """
 
+        # Remove the plots we produced
         filenames = ["chain-{0}.pdf".format(n) for n in xrange(1, self.n+1)]
         filenames.extend(["inference.pdf", "inference-all.pdf"])
 
-        filenames.extend(["model.yaml", "grid-points.pickle", "dispersion.memmap", "fluxes.memmap"])
+        # Remove the model filenames
+        filenames.extend(["inference-model.yaml", "inference-dispersion.memmap", "inference-flux.memmap",
+            "inference-grid-points.pickle", "test-inference-data.tar"])
 
         map(os.unlink, filenames)
+
+
+if __name__ == "__main__":
+    dat_inference = InferenceTest()
+    dat_inference.setUp()
+    dat_inference.runTest()
+    dat_inference.tearDown()
